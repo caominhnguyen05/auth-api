fa223c4837df0a9ec25acae305d75113d35c0470
Cao Minh Nguyen Cao Minh <c.m.nguyen-1@student.tudelft.nl>
2025-07-07 11:56:53 +0200

    Add verification code methods

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,13 @@
 const jwt = require("jsonwebtoken");
-const { signupSchema, signinSchema } = require("../middlewares/validator");
+const {
+  signupSchema,
+  signinSchema,
+  accceptCodeSchema,
+} = require("../middlewares/validator");
 const User = require("../models/usersModel");
-const { doHash, doHashValidation } = require("../utils/hashing");
+const { doHash, doHashValidation, hmacProcess } = require("../utils/hashing");
+const transport = require("../middlewares/sendMail");
+const { exist } = require("joi");
 
 exports.signup = async (req, res) => {
   const { email, password } = req.body;
@@ -97,4 +103,111 @@ exports.signout = async (req, res) => {
     .clearCookie("Authorization")
     .status(200)
     .json({ success: true, message: "Logged out successfully" });
+};
+
+exports.sendVerificationCode = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User does not exist!" });
+    }
+
+    if (existingUser.verified) {
+      return res
+        .status(400)
+        .json({ success: false, message: "You are already verified!" });
+    }
+
+    const codeValue = Math.floor(Math.random() * 1000000).toString();
+    let info = await transport.sendMail({
+      from: process.env.NODE_CODE_SENDING_EMAIL_ADDRESS,
+      to: existingUser.email,
+      subject: "Verification code",
+      html: "<h1>" + codeValue + "</h1>",
+    });
+
+    if (info.accepted[0] == existingUser.email) {
+      const hashedCodeValue = hmacProcess(
+        codeValue,
+        process.env.HMAC_VERIFICATION_CODE_SECRET
+      );
+      existingUser.verificationCode = hashedCodeValue;
+      existingUser.verificationCodeValidation = Date.now();
+      await existingUser.save();
+      return res.status(200).json({ success: true, message: "Code sent!" });
+    }
+    res.status(400).json({ success: false, message: "Code sent failed!" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.verifyVerificationCode = async (req, res) => {
+  const { email, providedCode } = req.body;
+  try {
+    const { error, value } = accceptCodeSchema.validate({
+      email,
+      providedCode,
+    });
+    if (error) {
+      return res
+        .status(401)
+        .json({ success: false, message: error.details[0].message });
+    }
+
+    const codeValue = providedCode.toString();
+    const existingUser = await User.findOne({ email }).select(
+      "+verificationCode +verificationCodeValidation"
+    );
+
+    console.log(existingUser);
+    if (!existingUser) {
+      return res
+        .status(401)
+        .json({ success: false, message: "User does not exist!" });
+    }
+    if (existingUser.verified) {
+      return res
+        .status(400)
+        .json({ success: false, mesasge: "You are already verified!" });
+    }
+
+    if (
+      !existingUser.verificationCode ||
+      !existingUser.verificationCodeValidation
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, mesasge: "Something is wrong with the code" });
+    }
+
+    if (Date.now() - existingUser.verificationCodeValidation > 5 * 60 * 1000) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Code has been expired!" });
+    }
+
+    const hashedCodeValue = hmacProcess(
+      codeValue,
+      process.env.HMAC_VERIFICATION_CODE_SECRET
+    );
+
+    if (hashedCodeValue === existingUser.verificationCode) {
+      existingUser.verified = true;
+      existingUser.verificationCode = undefined;
+      existingUser.verificationCodeValidation = undefined;
+      await existingUser.save();
+      return res
+        .status(200)
+        .json({ success: false, message: "Your account has been verified!" });
+    }
+    return res
+      .status(400)
+      .json({ success: false, message: "Unexpected error occured!" });
+  } catch (error) {
+    console.log(error);
+  }
 };
