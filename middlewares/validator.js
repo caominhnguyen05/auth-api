60878dca93b159d6e0096f243101be612eb88694
Cao Minh Nguyen Cao Minh <c.m.nguyen-1@student.tudelft.nl>
2025-07-07 16:37:07 +0200

    Add schema for forgot password

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -55,3 +55,20 @@ exports.changePasswordSchema = Joi.object({
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
     ),
 });
+
+exports.acceptFPCodeSchema = Joi.object({
+  email: Joi.string()
+    .min(6)
+    .max(60)
+    .required()
+    .email({
+      tlds: { allow: ["com", "net"] },
+    }),
+  providedCode: Joi.number().required(),
+
+  newPassword: Joi.string()
+    .required()
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    ),
+});
