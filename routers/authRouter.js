f398c0c19368bed1120a4cebb151190f6be90e61
Cao Minh Nguyen Cao Minh <c.m.nguyen-1@student.tudelft.nl>
2025-07-07 10:44:25 +0200

    Fix sign in URL

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -3,6 +3,6 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/signup", authController.signup);
-router.post("/sigin", authController.signin);
+router.post("/signin", authController.signin);
 
 module.exports = router;
