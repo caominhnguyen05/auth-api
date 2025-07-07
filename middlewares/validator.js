1219f9636cd346cc6dec5214bc849ba41a6a481a
Cao Minh Nguyen Cao Minh <c.m.nguyen-1@student.tudelft.nl>
2025-07-07 11:57:37 +0200

    Add hashing and validator schema for verification code operations

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -31,3 +31,14 @@ exports.signinSchema = Joi.object({
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
     ),
 });
+
+exports.accceptCodeSchema = Joi.object({
+  email: Joi.string()
+    .min(6)
+    .max(60)
+    .required()
+    .email({
+      tlds: { allow: ["com", "net"] },
+    }),
+  providedCode: Joi.number().required(),
+});
