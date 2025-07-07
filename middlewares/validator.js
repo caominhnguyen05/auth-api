439e797bf88f46d1f1a41a0ba12baf2204082de4
Cao Minh Nguyen Cao Minh <c.m.nguyen-1@student.tudelft.nl>
2025-07-07 10:29:40 +0200

    Add validator for email and password

---
+++ middlewares/validator.js
@@ -0,0 +1,33 @@
+const Joi = require("joi");
+
+exports.signupSchema = Joi.object({
+  email: Joi.string()
+    .min(6)
+    .max(60)
+    .required()
+    .email({
+      tlds: { allow: ["com", "net"] },
+    }),
+
+  password: Joi.string()
+    .required()
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    ),
+});
+
+exports.signinSchema = Joi.object({
+  email: Joi.string()
+    .min(6)
+    .max(60)
+    .required()
+    .email({
+      tlds: { allow: ["com", "net"] },
+    }),
+
+  password: Joi.string()
+    .required()
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    ),
+});
